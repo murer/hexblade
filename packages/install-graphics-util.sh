f167e7d75e664e560342417f9f7dc53a16a0ed76
murer <murer@dextra-sw.com>
2020-01-20 19:08:57 -0300

    x

--- packages/install-graphics-util.sh
+++ packages/install-graphics-util.sh
@@ -2,4 +2,4 @@
 
 [[ "x$UID" != "x0" ]]
 
-sudo apt-get -y install firefox gedit gmrun lxterminal gedit gnome-screenshot terminator pcmanfm
+sudo apt-get -y install firefox gedit lxterminal gedit gnome-screenshot pcmanfm network-manager-gnome

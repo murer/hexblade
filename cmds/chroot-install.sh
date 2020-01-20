68f48d7bf819dc4f62df1d7438f0cdb640fbbe34
murer <murer@dextra-sw.com>
2020-01-20 19:04:25 -0300

    x

--- cmds/chroot-install.sh
+++ cmds/chroot-install.sh
@@ -40,5 +40,5 @@ arch-chroot /mnt/installer apt-get -y install grub-efi
 
 cp -R target/config/etc.post/* /mnt/installer/etc
 sudo arch-chroot /mnt/installer update-grub
-sudo arch-chroot /mnt/installer grub-install /dev/sda
+sudo arch-chroot /mnt/installer grub-install "$(cat target/config/grub.dev)"
 sudo arch-chroot /mnt/installer update-initramfs -u -k all

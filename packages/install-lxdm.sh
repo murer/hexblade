f0fe6f1044d4297675b504d161b63f3e9358656b
murer <murer@dextra-sw.com>
2020-09-14 22:44:04 -0300

    x

--- packages/install-lxdm.sh
+++ packages/install-lxdm.sh
@@ -1,4 +1,14 @@
 #!/bin/bash -xe
 
+cd "$(dirname "$0")"
+pwd
+
 sudo apt -y install lxdm
 
+if [ ! -f /etc/lxdm.original.tar.gz ]; then
+	cd /etc
+	sudo tar czf lxdm.original.tar.gz lxdm/*
+	cd -
+fi
+
+sudo cp -Rv lxdm/* /etc/lxdm

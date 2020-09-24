f1143f86ec8f49ba92d63c6588e8016dda73a900
murer <murer@dextra-sw.com>
2020-09-24 11:26:00 -0300

    x

--- cmds/chroot-live.sh
+++ cmds/chroot-live.sh
@@ -5,34 +5,4 @@
 cd "$(dirname "$0")/.."
 pwd
 
-cp -R target/config/etc.pre/* /mnt/installer/etc
-
 arch-chroot /mnt/installer ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
-arch-chroot /mnt/installer locale-gen en_US.UTF-8
-arch-chroot /mnt/installer dpkg-reconfigure -f non-interactive tzdata
-arch-chroot /mnt/installer dpkg-reconfigure keyboard-configuration
-
-[[ -d "/mnt/installer/home/$(cat target/config/user/user.txt)" ]] || \
-  arch-chroot /mnt/installer useradd -u 1000 -m -G adm,cdrom,sudo,dip,plugdev -s /bin/bash "$(cat target/config/user/user.txt)" -p "$(cat target/config/user/pass.txt)" || true
-
-arch-chroot /mnt/installer apt -y update
-#arch-chroot /mnt/installer apt -y upgrade
-arch-chroot /mnt/installer apt -y install ubuntu-standard \
-  language-pack-en-base \
-  software-properties-common \
-  vim wget curl openssl git vim \
-  nmap netcat pv zip connect-proxy tcpdump bc \
-  network-manager
-
-sudo tee /mnt/installer/etc/netplan/01-netcfg.yaml <<-EOF
-network:
-  version: 2
-  renderer: NetworkManager
-EOF
-
-rm -rf "/mnt/installer/home/$(cat target/config/user/user.txt)/hex"
-cp -R "." "/mnt/installer/home/$(cat target/config/user/user.txt)/hex"
-arch-chroot /mnt/installer chown -R "$(cat target/config/user/user.txt):$(cat target/config/user/user.txt)" "/home/$(cat target/config/user/user.txt)"
-
-arch-chroot /mnt/installer apt -y install linux-image-generic linux-headers-generic
-arch-chroot /mnt/installer apt -y install cryptsetup lvm2
--- cmds/strap.sh
+++ cmds/strap.sh
@@ -3,3 +3,10 @@
 [[ "x$UID" == "x0" ]]
 
 debootstrap bionic /mnt/installer
+
+# debootstrap \
+#     --arch=amd64 \
+#     --variant=minbase \
+#     bionic \
+#     $HOME/live-ubuntu-from-scratch/chroot \
+#     http://us.archive.ubuntu.com/ubuntu/

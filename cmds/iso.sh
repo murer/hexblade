35e4aed90d141a32a88ea7545198049e971cad1d
murer <murer@dextra-sw.com>
2020-09-24 11:48:58 -0300

    x

--- cmds/iso.sh
+++ cmds/iso.sh
@@ -53,3 +53,66 @@ sed -i '/casper/d' image/casper/filesystem.manifest-desktop
 sed -i '/discover/d' image/casper/filesystem.manifest-desktop
 sed -i '/laptop-detect/d' image/casper/filesystem.manifest-desktop
 sed -i '/os-prober/d' image/casper/filesystem.manifest-desktop
+
+mksquashfs chroot image/casper/filesystem.squashfs
+printf $(du -sx --block-size=1 chroot | cut -f1) > image/casper/filesystem.size
+
+cat > image/README.diskdefines <<-EOF
+#define DISKNAME hex
+#define TYPE  binary
+#define TYPEbinary  1
+#define ARCH  amd64
+#define ARCHamd64  1
+#define DISKNUM  1
+#define DISKNUM1  1
+#define TOTALNUM  0
+#define TOTALNUM0  1
+EOF
+
+grub-mkstandalone \
+   --format=x86_64-efi \
+   --output=isolinux/bootx64.efi \
+   --locales="" \
+   --fonts="" \
+   "boot/grub/grub.cfg=isolinux/grub.cfg"
+
+cd isolinux
+dd if=/dev/zero of=efiboot.img bs=1M count=10
+mkfs.vfat efiboot.img
+LC_CTYPE=C mmd -i efiboot.img efi efi/boot
+LC_CTYPE=C mcopy -i efiboot.img ./bootx64.efi ::efi/boot/
+
+grub-mkstandalone \
+   --format=i386-pc \
+   --output=isolinux/core.img \
+   --install-modules="linux16 linux normal iso9660 biosdisk memdisk search tar ls" \
+   --modules="linux16 linux normal iso9660 biosdisk search" \
+   --locales="" \
+   --fonts="" \
+   "boot/grub/grub.cfg=isolinux/grub.cfg"
+
+cat /usr/lib/grub/i386-pc/cdboot.img isolinux/core.img > isolinux/bios.img
+
+/bin/bash -c "(find . -type f -print0 | xargs -0 md5sum | grep -v "\./md5sum.txt" > md5sum.txt)"
+
+xorriso \
+   -as mkisofs \
+   -iso-level 3 \
+   -full-iso9660-filenames \
+   -volid "Ubuntu from scratch" \
+   -eltorito-boot boot/grub/bios.img \
+   -no-emul-boot \
+   -boot-load-size 4 \
+   -boot-info-table \
+   --eltorito-catalog boot/grub/boot.cat \
+   --grub2-boot-info \
+   --grub2-mbr /usr/lib/grub/i386-pc/boot_hybrid.img \
+   -eltorito-alt-boot \
+   -e EFI/efiboot.img \
+   -no-emul-boot \
+   -append_partition 2 0xef isolinux/efiboot.img \
+   -output "../hex.iso" \
+   -graft-points \
+      "." \
+      /boot/grub/bios.img=isolinux/bios.img \
+      /EFI/efiboot.img=isolinux/efiboot.img

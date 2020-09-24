66607761d6b650976bf72bf64196e374d63ccaf1
murer <murer@dextra-sw.com>
2020-09-24 11:45:34 -0300

    x

--- cmds/iso.sh
+++ cmds/iso.sh
@@ -4,5 +4,52 @@
 
 cd /mnt
 mkdir -p image/{casper,isolinux,install}
-sudo cp installer/boot/vmlinuz-**-**-generic image/casper/vmlinuz
-sudo cp installer/boot/initrd.img-**-**-generic image/casper/initrd
+cp installer/boot/vmlinuz-**-**-generic image/casper/vmlinuz
+cp installer/boot/initrd.img-**-**-generic image/casper/initrd
+
+touch image/ubuntu
+
+cat > image/isolinux/grub.cfg <<-EOF
+
+search --set=root --file /ubuntu
+
+insmod all_video
+
+set default="0"
+set timeout=30
+
+menuentry "Try Ubuntu FS without installing" {
+   linux /casper/vmlinuz boot=casper quiet splash ---
+   initrd /casper/initrd
+}
+
+menuentry "Install Ubuntu FS" {
+   linux /casper/vmlinuz boot=casper only-ubiquity quiet splash ---
+   initrd /casper/initrd
+}
+
+menuentry "Check disc for defects" {
+   linux /casper/vmlinuz boot=casper integrity-check quiet splash ---
+   initrd /casper/initrd
+}
+
+menuentry "Test memory Memtest86+ (BIOS)" {
+   linux16 /install/memtest86+
+}
+
+menuentry "Test memory Memtest86 (UEFI, long load time)" {
+   insmod part_gpt
+   insmod search_fs_uuid
+   insmod chain
+   loopback loop /install/memtest86
+   chainloader (loop,gpt1)/efi/boot/BOOTX64.efi
+}
+EOF
+
+arch-chroot /mnt/installer dpkg-query -W --showformat='${Package} ${Version}\n' | tee image/casper/filesystem.manifest
+cp -v image/casper/filesystem.manifest image/casper/filesystem.manifest-desktop
+sed -i '/ubiquity/d' image/casper/filesystem.manifest-desktop
+sed -i '/casper/d' image/casper/filesystem.manifest-desktop
+sed -i '/discover/d' image/casper/filesystem.manifest-desktop
+sed -i '/laptop-detect/d' image/casper/filesystem.manifest-desktop
+sed -i '/os-prober/d' image/casper/filesystem.manifest-desktop

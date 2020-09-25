fbfbf477c8efcbbdf0c6d5aed6cfdf61bbb6c854
murer <murer@dextra-sw.com>
2020-09-24 21:28:11 -0300

    x

--- cmds/chroot-openbox.sh
+++ cmds/chroot-openbox.sh
@@ -13,7 +13,7 @@ arch-chroot /mnt/installer chown -R "$hex_user:$hex_user" "/home/$hex_user"
 
 echo "$hex_user ALL=(ALL) NOPASSWD: ALL" > /mnt/installer/etc/sudoers.d/tmp
 
-arch-chroot \
-  -u "$hex_user:$hex_user" \
-  /mnt/installer \
-  "/home/$hex_user/hex/packages/install-graphics-util.sh"
+arch-chroot -u "$hex_user:$hex_user" /mnt/installer "/home/$hex_user/hex/packages/install-graphics-util.sh"
+arch-chroot -u "$hex_user:$hex_user" /mnt/installer "/home/$hex_user/hex/packages/install-sound.sh"
+arch-chroot -u "$hex_user:$hex_user" /mnt/installer "/home/$hex_user/hex/packages/install-openbox.sh"
+arch-chroot -u "$hex_user:$hex_user" /mnt/installer "/home/$hex_user/hex/packages/install-lxdm.sh"

1733e7048adb1b205f9ed2382fe4c9c9071201de
murer <murer@dextra-sw.com>
2020-09-25 18:01:26 -0300

    x

--- cmds/installer-prepare.sh
+++ cmds/installer-prepare.sh
@@ -2,13 +2,13 @@
 
 [[ "x$UID" == "x0" ]]
 
-apt -y update
-apt -y install \
+apt -y update -o Dpkg::Progress-Fancy="0"
+apt -y install -o Dpkg::Progress-Fancy="0" \
   software-properties-common
 
 apt-add-repository universe
 
-apt install -y \
+apt install -y -o Dpkg::Progress-Fancy="0" \
   gdisk fdisk gpart \
   cryptsetup \
   debootstrap debconf-utils \

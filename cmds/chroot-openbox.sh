d9165db606a7e6b9349b5b2d6b9e7b6756d6dd8a
murer <murer@dextra-sw.com>
2020-09-24 21:19:33 -0300

    x

--- cmds/chroot-openbox.sh
+++ cmds/chroot-openbox.sh
@@ -9,7 +9,7 @@ hex_user="$(cat target/config/user/user.txt)"
 
 rm -rf "/mnt/installer/home/$hex_user/hex"
 cp -R "." "/mnt/installer/home/$hex_user/hex"
-arch-chroot -/mnt/installer chown -R "$hex_user:$hex_user" "/home/$hex_user"
+arch-chroot /mnt/installer chown -R "$hex_user:$hex_user" "/home/$hex_user"
 
 arch-chroot \
   -u "$hex_user:$hex_user" \

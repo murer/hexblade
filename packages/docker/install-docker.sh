3f1d49b9a921bda55f25a78db40685dd0e808365
murer <murer@dextra-sw.com>
2020-09-30 18:53:43 -0300

    x

--- packages/docker/install-docker.sh
+++ packages/docker/install-docker.sh
@@ -9,10 +9,13 @@ apt install -y \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" >> /etc/apt/sources.list.d/docker.list
+echo "# deb-src [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" >> /etc/apt/sources.list.d/docker.list
+
+# add-apt-repository \
+#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#    $(lsb_release -cs) \
+#    stable"
 
 apt -y update
 

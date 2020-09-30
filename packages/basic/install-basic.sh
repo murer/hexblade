132948033acc691aae4e9c5f581917f228c99013
murer <murer@dextra-sw.com>
2020-09-30 18:54:40 -0300

    x

--- packages/basic/install-basic.sh
+++ packages/basic/install-basic.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")/.."
 pwd
 
 ./graphics/install-graphics.sh
-./graphics/install-firefox.sh
+./firefox/install-firefox.sh
 ./sound/install-sound.sh
 ./openbox/install-openbox.sh
 ./lxdm/install-lxdm.sh

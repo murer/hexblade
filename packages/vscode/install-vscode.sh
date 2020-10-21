5692f1173517ade689e6f5af57ff6035a63927c1
murer <murer@dextra-sw.com>
2020-10-21 12:38:02 -0300

    x

--- packages/vscode/install-vscode.sh
+++ packages/vscode/install-vscode.sh
@@ -16,16 +16,3 @@ fi
 [[ -f /usr/local/bin/code ]] || ln -s /opt/VSCode-linux-x64/code /usr/local/bin/code
 
 cd -
-
-# https://go.microsoft.com/fwlink/?LinkID=620884
-
-
-# wget --progress=dot -e dotbytes=1M -c \
-#   'https://go.microsoft.com/fwlink/?LinkID=760868' \
-#   -O target/vscode.deb
-#
-# if ! code -v; then
-#   dpkg -i target/vscode.deb || true
-#   apt install -yf
-#   dpkg -i target/vscode.deb
-# fi

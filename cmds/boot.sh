d70318a6c41b2c2a0fb68a48cc983b5bb47fd3ef
murer <murer@dextra-sw.com>
2020-11-26 23:54:30 -0300

    .

--- cmds/boot.sh
+++ cmds/boot.sh
@@ -8,7 +8,7 @@ set -x
 
 [[ "x$hexblade_grub_dev" != "x" ]]
 
-DEBIAN_FRONTEND=noninteractive arch-chroot /mnt/hexblade/installer apt -y install linux-firmware linux-generic-hwe-18.04
+DEBIAN_FRONTEND=noninteractive arch-chroot /mnt/hexblade/installer apt -y install linux-firmware
 
 if [[ -d /mnt/hexblade/installer/boot/efi ]]; then
   arch-chroot /mnt/hexblade/installer apt -y install grub-efi

eadd1aca5da8b379f717c30b7fcef37bc50358c9
murer <murer@dextra-sw.com>
2020-11-27 00:47:01 -0300

    .

--- cmds/boot.sh
+++ cmds/boot.sh
@@ -8,8 +8,6 @@ set -x
 
 [[ "x$hexblade_grub_dev" != "x" ]]
 
-DEBIAN_FRONTEND=noninteractive arch-chroot /mnt/hexblade/installer apt -y install linux-firmware
-
 if [[ -d /mnt/hexblade/installer/boot/efi ]]; then
   arch-chroot /mnt/hexblade/installer apt -y install grub-efi
 fi

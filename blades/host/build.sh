d7a8fed0aca3e253f187e759c67a42a31a611e6b
murer <murer@dextra-sw.com>
2021-02-25 00:31:48 -0300

    x

--- blades/host/build.sh
+++ blades/host/build.sh
@@ -2,11 +2,18 @@
 
 export DEBIAN_FRONTEND="noninteractive"
 
-cmd_install() {
+cmd_deps() {
     ../../packages/virtualbox/install-virtualbox.sh
     ../../packages/openbox/install-openbox.sh
+}
 
+cmd_config() {
     cp -Rv etc/* /etc
+}
+
+cmd_install() {
+    cmd_deps
+    cmd_config
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"

a05760cb66cb9ce69d7b3187735a2a948a543868
murer <murer@dextra-sw.com>
2021-03-10 09:17:47 -0300

    x

---
+++ blades/firewall.sh
@@ -0,0 +1,12 @@
+# Set default chain policies
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+# Accept on localhost
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# Allow established sessions to receive traffic
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+

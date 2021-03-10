f6e617192cd08b088725f260682d3888ff91fb21
murer <murer@dextra-sw.com>
2021-03-10 11:15:24 -0300

    x

--- blades/host/firewall.sh
+++ blades/host/firewall.sh
@@ -7,6 +7,10 @@ iptables -P OUTPUT ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
+# Accept some ports
+iptables -A INPUT -p tcp -s 0.0.0.0/0 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -d 0.0.0.0/0 --sport 22 -m state --state ESTABLISHED -j ACCEPT
+
 # Allow established sessions to receive traffic
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 

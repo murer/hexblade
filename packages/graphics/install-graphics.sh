d7ac29cd1b10cd3ad61550f5f1b9e449de965c2a
murer <murer@dextra-sw.com>
2021-03-21 08:13:43 -0300

    x

--- packages/graphics/install-graphics.sh
+++ packages/graphics/install-graphics.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 
-apt -y install xterm mousepad xfce4-screenshooter
+apt -y install xterm mousepad xfce4-screenshooter caja-seahorse
 apt -y install --no-install-recommends network-manager-gnome

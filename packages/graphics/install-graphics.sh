03a8f2a414b5f5a2822c7a485f01032523925025
murer <murer@dextra-sw.com>
2021-03-21 10:54:54 -0300

    x

--- packages/graphics/install-graphics.sh
+++ packages/graphics/install-graphics.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
 
 apt -y install --no-install-recommends network-manager-gnome xterm mousepad xfce4-screenshooter
-apt -y install thunar
+apt -y install --no-install-suggests thunar thunar-archive-plugin thunar-media-tags-plugin 
+

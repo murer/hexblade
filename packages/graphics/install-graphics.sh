562daf2e4f9236253afaa9e89e17489cbc874c4f
murer <murer@dextra-sw.com>
2021-03-22 09:20:39 -0300

    x

--- packages/graphics/install-graphics.sh
+++ packages/graphics/install-graphics.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
-apt -y install --no-install-recommends network-manager-gnome xterm mousepad xfce4-screenshooter pcmanfm 
+apt -y install --no-install-recommends xterm mousepad xfce4-screenshooter pcmanfm 
 # thunar thunar-volman thunar-archive-plugin thunar-media-tags-plugin 
 
---
+++ packages/networkmanager/install-networkmanager.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -xe
+
+apt -y install --no-install-recommends network-manager-gnome
+

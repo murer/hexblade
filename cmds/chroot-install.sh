a9b30e5ece8f1fac51d31c7116596a9315068779
murer <murer@dextra-sw.com>
2021-03-25 12:08:21 +0000

    x

--- cmds/chroot-install.sh
+++ cmds/chroot-install.sh
@@ -45,7 +45,7 @@ arch-chroot /mnt/hexblade/installer chown -R "$hexblade_user:$hexblade_user" "/h
 #DEBIAN_FRONTEND=noninteractive arch-chroot /mnt/hexblade/installer apt -y install "linux-image-5.4.0-54-generic" "linux-headers-5.4.0-54-generic"
 #DEBIAN_FRONTEND=noninteractive arch-chroot /mnt/hexblade/installer apt -y install "linux-image-generic" "linux-headers-generic"
 #DEBIAN_FRONTEND=noninteractive arch-chroot /mnt/hexblade/installer apt -y install linux-generic-hwe-20.04
-DEBIAN_FRONTEND=noninteractive arch-chroot /mnt/hexblade/installer apt -y install --install-recommends linux-generic-hwe-20.04
+DEBIAN_FRONTEND=noninteractive arch-chroot /mnt/hexblade/installer apt -y install --install-recommends linux-generic
 
 if [[ "x$hexblade_dev_lvm" != "x" ]]; then
 	arch-chroot /mnt/hexblade/installer apt -y install cryptsetup lvm2

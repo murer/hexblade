c1851b66c5bb158325c260d4ebf3a98c670450f2
murer <murer@dextra-sw.com>
2021-03-26 08:25:23 -0300

    x

--- packages/graphics/graphics.sh
+++ packages/graphics/graphics.sh
@@ -1,27 +1,31 @@
 #!/bin/bash -xe
 
 cmd_xterm() {
-    apt -y install xterm
+    apt -y install --no-install-recommends xterm
 }
 
 cmd_mousepad() {
-    apt -y install mousepad
+    apt -y install --no-install-recommends mousepad
 }
 
 cmd_xfce4-screenshooter() {
-    apt -y install xfce4-screenshooter
+    apt -y install --no-install-recommends xfce4-screenshooter
 }
 
 cmd_pcmanfm() {
-    apt -y install pcmanfm
+    apt -y install --no-install-recommends pcmanfm
 }
 
 cmd_firefox() {
     apt -y install firefox
 }
 
+cmd_network-manager-gnome() {
+    apt -y install --no-install-recommends network-manager-gnome
+}
+
 cmd_thunar() {
-    apt -y install thunar thunar-volman thunar-archive-plugin thunar-media-tags-plugin 
+    apt -y install --no-install-recommends thunar thunar-volman thunar-archive-plugin thunar-media-tags-plugin 
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"
--- packages/standard/standard.sh
+++ packages/standard/standard.sh
@@ -1,19 +1,24 @@
 #!/bin/bash -xe
 
-cd "$(dirname "$0")/.."
-pwd
 
-./tools/tools.sh install
+cmd_install() {
+    ../tools/tools.sh install
 
-./graphics/graphics.sh xterm
-./graphics/graphics.sh mousepad
-./graphics/graphics.sh xfce4-screenshooter
-./graphics/graphics.sh pcmanfm
+    ../graphics/graphics.sh xterm
+    
+    ../lxterminal/lxterminal.sh install
+    
+    ../graphics/graphics.sh mousepad
+    ../graphics/graphics.sh xfce4-screenshooter
+    ../graphics/graphics.sh pcmanfm
+    ../graphics/graphics.sh firefox
 
-./lxterminal/lxterminal.sh install
+    ../graphics/graphics.sh network-manager-gnome
 
-./firefox/install-firefox.sh
-./pulseaudio/install-pulseaudio.sh
-./networkmanager/install-networkmanager.sh
-./openbox/install-openbox.sh
-./lxdm/install-lxdm.sh
+    ../pulseaudio/install-pulseaudio.sh
+    ../networkmanager/install-networkmanager.sh
+    ../openbox/install-openbox.sh
+    ../lxdm/install-lxdm.sh
+}
+
+cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"

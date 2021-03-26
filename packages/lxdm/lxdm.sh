b4484071051af1f41f97244c5bb2788c1a8a156d
murer <murer@dextra-sw.com>
2021-03-26 14:07:36 -0300

    x

--- packages/lxdm/lxdm.sh
+++ packages/lxdm/lxdm.sh
@@ -18,7 +18,25 @@ cmd_install() {
 }
 
 cmd_noautologin() {
-	
+	crudini --del /etc/lxdm/lxdm-hexblade.conf base autologin
+}
+
+cmd_autologin() {
+	hexblade_autologin="${1?'username'}"
+	crudini --set /etc/lxdm/lxdm-hexblade.conf base autologin "$hexblade_autologin"
+}
+
+cmd_tcplisten() {
+	hexblade_tcplisten="${1?'enable or disable'}"
+	if [[ "x$hexblade_tcplisten" == "xenable" ]]; then
+		crudini --set /etc/lxdm/lxdm-hexblade.conf server arg "/usr/bin/X +iglx -listen tcp"
+		crudini --set /etc/lxdm/lxdm-hexblade.conf server tcp_listen "1"
+	elif [[ "x$hexblade_tcplisten" == "xdisable" ]]; then
+		crudini --del /etc/lxdm/lxdm-hexblade.conf server arg
+		crudini --del /etc/lxdm/lxdm-hexblade.conf server tcp_listen
+	else
+		false
+	fi
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"

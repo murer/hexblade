b39db3003c6d47547b485098f6a829721f123cbc
murer <murer@dextra-sw.com>
2021-03-26 07:49:16 -0300

    x

--- packages/chrome/install-chrome.sh
+++ packages/chrome/install-chrome.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -xe
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list
-apt update
-apt -y install google-chrome-stable
+cmd_install() {
+    [[ "x$UID" == "x0" ]]
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list
+    apt update
+    apt -y install google-chrome-stable
+}
+
+cd "$(dirname "$0")/.."; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"
+

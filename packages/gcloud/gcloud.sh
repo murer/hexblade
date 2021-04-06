a9b66fca76d296bc99585a97bec40aaa0315d0ca
murer <murer@dextra-sw.com>
2021-04-06 09:38:10 -0300

    x

--- packages/gcloud/gcloud.sh
+++ packages/gcloud/gcloud.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 cmd_apt() {
-  apt install apt-transport-https ca-certificates gnupg
+  apt install -y apt-transport-https ca-certificates gnupg
   echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" > /etc/apt/sources.list.d/google-cloud-sdk.list
   curl "https://packages.cloud.google.com/apt/doc/apt-key.gpg" > /usr/share/keyrings/cloud.google.gpg
   #curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -

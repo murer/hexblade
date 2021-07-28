7954a10be62f927b4613df1fbea18a285304814f
murer <murer@dextra-sw.com>
2021-07-28 19:23:36 -0300

    x

--- install/libs/recipe-main.sh
+++ install/libs/recipe-main.sh
@@ -1,7 +1,14 @@
 
 cmd_recipe_basic() {
   hexblade_dev="${1?'target is required'}"
-  mkfs.ext4 -L ROOT "$hexblade_dev"
+  read -p 'Format (s/n): ' hexblade_recipe_format
+  if [[ "x$hexblade_recipe_format" == "xy" ]]; then
+    mkfs.ext4 -L ROOT "$hexblade_dev"
+  fi
   
   cmd_struct
+
+  [[ -d /mnt/hexblade/installer/bin ]] || cmd_basesys_strap
+  cmd_basesys_install
+  cmd_basesys_kernel
 }

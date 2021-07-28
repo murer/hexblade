4536178f4e22fdfea44dca15abb1f7ba24272f85
murer <murer@dextra-sw.com>
2021-07-28 17:54:21 -0300

    x

--- install/libs/user-main.sh
+++ install/libs/user-main.sh
@@ -5,7 +5,6 @@ function cmd_useradd() {
     if [ "x$hexblade_password" == "x" ]; then
         hexblade_password="$(openssl passwd -6)"
     fi
-    [[ -d "/mnt/hexblade/installer/home/$hexblade_username" ]] || \
-        arch-chroot /mnt/hexblade/installer useradd -m -G adm,cdrom,sudo,dip,plugdev -s /bin/bash "$hexblade_username" -p "$hexblade_password"
+    arch-chroot /mnt/hexblade/installer useradd -m -G adm,cdrom,sudo,dip,plugdev -s /bin/bash "$hexblade_username" -p "$hexblade_password"
 }
 

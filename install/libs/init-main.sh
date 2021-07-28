805cebc43ff3f6497978638bf6f40a99cd718ff0
murer <murer@dextra-sw.com>
2021-07-28 18:39:00 -0300

    x

--- install/libs/init-main.sh
+++ install/libs/init-main.sh
@@ -1,8 +1,8 @@
 
 cmd_prepare() {
-  sudo apt -y install software-properties-common
-  sudo apt-add-repository universe
-  sudo apt install -y --no-install-recommends \
+  apt -y install software-properties-common
+  apt-add-repository universe
+  apt install -y --no-install-recommends \
     gdisk fdisk gpart \
     cryptsetup \
     debootstrap debconf-utils \
--- install/libs/struct-main.sh
+++ install/libs/struct-main.sh
@@ -2,3 +2,11 @@
 cmd_struct() {
   sudo mkdir -p /mnt/hexblade/installer
 }
+
+cmd_struct_umount() {
+  sudo umount -R /mnt/hexblade/installer
+}
+
+cmd_fstab() {
+  genfstab -U /mnt/hexblade/installer | tee /mnt/hexblade/installer/etc/fstab
+}

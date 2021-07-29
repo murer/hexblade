36d6a8d02a935e076cfb724caf488ff20beb1241
murer <murer@dextra-sw.com>
2021-07-29 17:41:43 -0300

    x

--- install/resources/crypt/initramfs-tools/scripts/local-premount/hexblade_decrypt.sh
+++ install/resources/crypt/initramfs-tools/scripts/local-premount/hexblade_decrypt.sh
@@ -1,5 +1,7 @@
 #!/bin/sh 
 
+set -e
+
 PREREQ="btrfs"
 
 # Output pre-requisites
@@ -25,5 +27,23 @@ ls /secrets/parts/id-*.id | while read k; do
         cryptsetup open --key-file "/secrets/parts/key-$crypt_part_name.key" "/dev/disk/by-uuid/$(cat "$k")" "$crypt_part_name"
 done
 
-set +x
-sh
+
+if grep "localsync" /proc/cmdline; then
+	# mount -t btrfs -r -o subvol=@root /dev/mapper/MAINCRYPTED /syncsrc
+	# mount /dev/mapper/LOCAL-ROOT /syncdst
+	# rsync -a --delete /syncsrc/ /syncdst/
+	# hexblade_maincrypted_id="$(blkid -o value -s UUID /dev/mapper/MAINCRYPTED)"
+	# [[ "x$hexblade_maincrypted_id" != "x" ]]
+	# sed -e "s/UUID=$hexblade_maincrypted_id/$ROOT/g" /syncsrc/etc/fstab > /syncdst/etc/fstab 
+	# grep -v ^MAINCRYPTED /syncdst/etc/crypttab > /syncdst/etc/crypttab.tmp || true
+	# mv /syncdst/etc/crypttab.tmp /syncdst/etc/crypttab
+	# if grep "localsyncdebug" /proc/cmdline; then
+	# 	sh
+	# fi
+	# umount /syncdst
+	# umount /syncsrc
+	# cryptsetup close MAINCRYPTED
+        sh
+fi
+
+

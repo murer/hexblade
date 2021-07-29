35317a12ccdd8ef304955b2bb78c5c2408464dc1
murer <murer@dextra-sw.com>
2021-07-29 16:11:53 -0300

    x

--- install/libs/recipe-localsync-main.sh
+++ install/libs/recipe-localsync-main.sh
@@ -17,10 +17,27 @@ function cmd_recipe_localsync_from_backup() {
     cmd_btrfs_subvol_mount /dev/mapper/MAINCRYPTED secrets /mnt/hexblade/secrets
     cmd_btrfs_subvol_mount /dev/mapper/MAINCRYPTED root /mnt/hexblade/installer
 
+    if [[ "x$hexblade_recipe_grub_dev" ]]; then
+        cmd_efi_mount_if_needed "$hexblade_recipe_grub_dev"
+    fi
+
     cmd_crypt_format_with_file "$hexblade_recipe_local_dev" LOCALCRYPTED
     cmd_lvm_format /dev/mapper/LOCALCRYPTED LOCAL
-    cmd_lvm_add LOCAL SWAP 2G
-    cmd_lvm_add LOCAL ROOT 8G
+    cmd_lvm_add LOCAL SWAP "$hexblade_recipe_swap_size"
+    cmd_lvm_add LOCAL ROOT "$hexblade_recipe_local_root_size"
     cmd_lvm_add LOCAL DATA '100%FREE'
+
+    mkswap -L SWAP /dev/mapper/LOCAL-SWAP
+    mkfs.ext4 -L ROOT /dev/mapper/LOCAL-ROOT
+    mkfs.ext4 -L DATA /dev/mapper/LOCAL-DATA
+    swapon /dev/mapper/LOCAL-SWAP
     
+    rsync -a --delete /mnt/hexblade/backup/ /mnt/hexblade/installer/
+    
+    mkdir -p /mnt/hexblade/installer/localdata
+    mount /dev/mapper/LOCAL-DATA /mnt/hexblade/installer/localdata
+
+    cmd_crypt_tab
+    cmd_struct_fstab
+    cmd_boot "$hexblade_recipe_grub_dev"
 }

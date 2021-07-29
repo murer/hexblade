776adc6fb6cf1510204e6e65b5468d3679b84756
murer <murer@dextra-sw.com>
2021-07-29 11:01:44 -0300

    x

--- install/libs/btrfs-main.sh
+++ install/libs/btrfs-main.sh
@@ -11,3 +11,11 @@ function cmd_btrfs_mount() {
     mkdir -p "/mnt/hexblade/btrfs/$hexblade_btrfs_id"
     mount -t btrfs -o compress=lzo "$hexblade_btrfs_dev" "/mnt/hexblade/btrfs/$hexblade_btrfs_id"
 }
+
+function cmd_btrfs_subvol_create() {
+    hexblade_btrfs_dev="${1?'hexblade_btrfs_dev is required'}"
+    hexblade_btrfs_subvol_name="${2?'hexblade_btrfs_subvol_name is required'}"
+
+    hexblade_btrfs_id="$(blkid -o value -s UUID "$hexblade_btrfs_dev")"
+    btrfs subvolume create "/mnt/hexblade/btrfs/$hexblade_btrfs_id/@$hexblade_btrfs_subvol_name"
+}

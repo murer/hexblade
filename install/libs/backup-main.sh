c2196c0027609a464d6476167b07c4f2faab11e1
murer <murer@dextra-sw.com>
2021-07-30 00:28:14 -0300

    x

--- install/libs/backup-main.sh
+++ install/libs/backup-main.sh
@@ -2,7 +2,7 @@
 function cmd_backup() {
     mkdir -p /mnt/hexblade/backup
     cd /mnt/hexblade/installer
-    rsync -a --delete /mnt/hexblade/installer/ /mnt/hexblade/backup/
+    rsync -a --delete -x /mnt/hexblade/installer/ /mnt/hexblade/backup/
 }
 
 function cmd_backup_restore() {

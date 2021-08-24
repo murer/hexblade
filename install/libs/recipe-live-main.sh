345692eacbccc28152805db02ad9d02f18294c98
murer <murer@dextra-sw.com>
2021-08-23 22:53:28 -0300

    x

--- install/libs/recipe-live-main.sh
+++ install/libs/recipe-live-main.sh
@@ -1,6 +1,20 @@
 
 cmd_recipe_live() {
   
+  [[ -d /mnt/hexblade/config ]] || cmd_config
+
+  cmd_struct
+  # findmnt /mnt/hexblade/installer || mount -t tmpfs -o size=6g tmpfs /mnt/hexblade/installer
+  # mkdir -p /mnt/hexblade/installer/boot/efi
+
+  [[ -d /mnt/hexblade/installer/bin ]] || cmd_basesys_strap
+  cmd_basesys_install
+  cmd_struct_fstab 
+  cmd_basesys_kernel
+  cmd_keyboard
+
+  cmd_user_add "ubuntu"
+
   cmd_live_install
   cmd_live_compress
   cmd_live_iso
--- install/libs/user-main.sh
+++ install/libs/user-main.sh
@@ -5,8 +5,14 @@ function cmd_user_add() {
         hexblade_password="$2"
         if [ "x$hexblade_password" == "x" ]; then
             hexblade_password="$(openssl passwd -6)"
+        elif [ "x$hexblade_password" == "xplain" ] || [ "x$3" != "x" ]; then
+            hexblade_password="$(echo -n \"$hexblade_password\" | openssl passwd -6 -stdin)"
         fi
-        arch-chroot /mnt/hexblade/installer useradd -m -G adm,cdrom,sudo,dip,plugdev -s /bin/bash "$hexblade_username" -p "$hexblade_password"
+        if [ "x$hexblade_password" == "nopass" ]; then
+            arch-chroot /mnt/hexblade/installer useradd -m -G adm,cdrom,sudo,dip,plugdev -s /bin/bash "$hexblade_username"
+        else
+            arch-chroot /mnt/hexblade/installer useradd -m -G adm,cdrom,sudo,dip,plugdev -s /bin/bash "$hexblade_username" -p "$hexblade_password"
+        fi
     fi
 }
 

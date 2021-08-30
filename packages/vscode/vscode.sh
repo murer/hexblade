813cdb2a84a3ae1bffdf627c4cd38a00654586b5
murer <murer@dextra-sw.com>
2021-08-30 15:34:25 -0300

    x

--- packages/vscode/vscode.sh
+++ packages/vscode/vscode.sh
@@ -1,24 +1,11 @@
 #!/bin/bash -xe
 
-cmd_clean() {
-  rm -rf /opt/vscode.tar.gz || true
-  [[ ! -f /opt/vscode.tar.gz ]]
-}
-
 cmd_install() {
-  cd /opt
-
-  if [[ ! -f VSCode-linux-x64/code ]]; then
-    wget --progress=dot -e dotbytes=1M -c \
-      'https://go.microsoft.com/fwlink/?LinkID=620884' \
-      -O vscode.tar.gz
-    tar xzf vscode.tar.gz
-    cmd_clean
-  fi
-
-  [[ -f /usr/local/bin/code ]] || ln -s /opt/VSCode-linux-x64/code /usr/local/bin/code
-
-  cd -
+	apt install -y apt-transport-https
+	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/packages.microsoft.gpg
+	echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list
+	apt update
+	apt install -y code
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"

1dc2639b75f53143eda13411ed08c188759ea367
murer <murer@dextra-sw.com>
2021-09-03 17:35:57 -0300

    x

--- packages/templates/hexes/hexes.sh
+++ packages/templates/hexes/hexes.sh
@@ -8,6 +8,9 @@ cmd_install() {
 
   #../../sound/sound.sh tcp
   #../../lxdm/lxdm.sh tcplisten enable
+
+  cp src/hexes-*.sh /usr/local/bin
+  chmod +x /usr/local/bin/hexes-*.sh
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"

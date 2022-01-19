f9d6f5de6d15fa13ace9fca91af1a5e6e7700742
murer <murer@dextra-sw.com>
2022-01-18 21:16:09 -0300

    x

---
+++ src/packs/100-ssh/ssh.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -xe
+
+cmd_enc() {
+  _basedir="$(pwd)"
+  [[ ! -f "$_basedir/ssh-key.tar.gz.pgp" ]]
+  cd "$HOME"
+  [[ -d .ssh ]]
+  tar czf - .ssh | gpg --batch -c --armor -o "$_basedir/ssh-key.tar.gz.pgp" -
+  cd -
+}
+
+cmd_dec() {
+  _basedir="$(pwd)"
+  cd "$HOME"
+  [[ ! -d .ssh ]]
+  gpg --batch -d "$_basedir/ssh-key.tar.gz.pgp" | tar xzf -
+  cd -
+}
+
+cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"

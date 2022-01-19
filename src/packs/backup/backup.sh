fa1a5be5b5321a66694feee114d02aaa49433476
murer <murer@dextra-sw.com>
2022-01-19 08:59:41 -0300

    x

--- src/packs/backup/backup.sh
+++ src/packs/backup/backup.sh
@@ -2,7 +2,26 @@
 
 _hex_backup_server="pyrata@s.murerz.com"
 
-cmd_push() {
+cmd_rcreate() {
+    _hex_backup_name="${1?'backup name is required'}"
+    if ssh "$_hex_backup_server" ls "hexblade/backup/$_hex_backup_name"; then
+        echo "backup already exists" 2>&1
+        false
+    fi
+    ssh "$_hex_backup_server" mkdir -p "hexblade/backup/$_hex_backup_name"
+    echo "$_hex_backup_name" | ssh "$_hex_backup_server" tee "hexblade/backup/$_hex_backup_name/name.txt"
+}
+
+cmd_rdelete_force() {
+    _hex_backup_name="${1?'backup name is required'}"
+    ssh "$_hex_backup_server" rm -rvf "hexblade/backup/$_hex_backup_name"
+}
+
+# cmd_rlist() {
+
+# }
+
+cmd_rpush() {
     _hex_backup_name="${1?'backup name is required'}"
     ssh "$_hex_backup_server" ls "hexblade/backup/$_hex_backup_name"
     cd /mnt/hexblade/basesys

899235a77a41fe81375c5381eddae16b4abde9b5
murer <murer@dextra-sw.com>
2022-01-19 11:00:17 -0300

    x

--- src/packs/backup/backup.sh
+++ src/packs/backup/backup.sh
@@ -20,7 +20,7 @@ cmd_rcreate() {
 
     [[ -f "$HOME/.ssh/id_rsa" ]]
 
-    while ! ssh "$_hex_backup_server" whoami; do sleep 5; done 
+    while ! ssh -o ConnectTimeout=10 "$_hex_backup_server" whoami; do sleep 5; done 
 
     if [[ "x$_hex_backup_from_version" == "x0" ]]; then
         if ssh "$_hex_backup_server" ls "hexblade/backup/$_hex_backup_name"; then
@@ -50,12 +50,12 @@ cmd_rcreate() {
     
     ssh "$_hex_backup_server" mv -v "hexblade/backup/$_hex_backup_name/$_hex_backup_to_version/data.tar.gz.gpg.tmp" "hexblade/backup/$_hex_backup_name/$_hex_backup_to_version/data.tar.gz.gpg"
 
-    while ! scp "rbak/$_hex_backup_to_version/cursor.sng" "$_hex_backup_server:hexblade/backup/$_hex_backup_name/$_hex_backup_to_version/cursor.sng"; do sleep 5; done
+    while ! scp -o ConnectTimeout=10 "rbak/$_hex_backup_to_version/cursor.sng" "$_hex_backup_server:hexblade/backup/$_hex_backup_name/$_hex_backup_to_version/cursor.sng"; do sleep 5; done
 }
 
 cmd_rdelete_force() {
     _hex_backup_name="${1?'backup name is required'}"
-    while ! ssh "$_hex_backup_server" whoami; do sleep 5; done 
+    while ! ssh -o ConnectTimeout=10 "$_hex_backup_server" whoami; do sleep 5; done 
     ssh "$_hex_backup_server" rm -rvf "hexblade/backup/$_hex_backup_name"
 }
 

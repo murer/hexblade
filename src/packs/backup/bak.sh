50af3c6e1da7351afab71c67d9289ce7aa273d26
murer <murer@dextra-sw.com>
2022-01-20 18:25:15 -0300

    x

--- src/packs/backup/bak.sh
+++ src/packs/backup/bak.sh
@@ -4,19 +4,19 @@ function cmd_ssh() {
     ssh -o ConnectTimeout=5 -o ConnectionAttempts=1000 "$@"
 }
 
-function cmd_bak_rsync_create() {
+function cmd_rsync_create() {
     local _hex_bak_version="${1?'_hex_bak_version is required'}"
     sudo mkdir -p "/mnt/hexblade/bak/$_hex_bak_version"
     sudo rsync -a --delete -x --info=progress2 /mnt/hexblade/basesys/ "/mnt/hexblade/bak/$_hex_bak_version/"
 }
 
-function cmd_bak_rsync_restore() {
+function cmd_rsync_restore() {
     local _hex_bak_version="${1?'_hex_bak_version is required'}"
     sudo mkdir -p /mnt/hexblade/basesys/
     sudo rsync -a --delete -x --info=progress2 "/mnt/hexblade/bak/$_hex_bak_version/" /mnt/hexblade/basesys/
 }
 
-function cmd_bak_remote_create() {
+function cmd_remote_create() {
     local _hex_bak_server="${1?'_hex_bak_server is required, like user@host'}"
     local _hex_bak_name="${2?'_hex_bak_name is required'}"
     local _hex_bak_version="${3?'_hex_bak_version is required'}"
@@ -33,7 +33,7 @@ function cmd_bak_remote_create() {
     cd -
 }
 
-function cmd_bak_remote_restore() {
+function cmd_remote_restore() {
     local _hex_bak_server="${1?'_hex_bak_server is required, like user@host'}"
     local _hex_bak_name="${2?'_hex_bak_name is required'}"
     local _hex_bak_version="${3?'_hex_bak_version is required'}"
@@ -46,24 +46,30 @@ function cmd_bak_remote_restore() {
     cd -
 }
 
-function cmd_bak_remote_delete_backup() {
+function cmd_remote_delete_backup() {
     local _hex_bak_server="${1?'_hex_bak_server is required, like user@host'}"
     local _hex_bak_name="${2?'_hex_bak_name is required'}"
     cmd_ssh "$_hex_bak_server" rm -rf "hexblade/bak/$_hex_bak_name"
 }
 
-function cmd_bak_remote_delete_version() {
+function cmd_remote_delete_version() {
     local _hex_bak_server="${1?'_hex_bak_server is required, like user@host'}"
     local _hex_bak_name="${2?'_hex_bak_name is required'}"
     local _hex_bak_version="${3?'_hex_bak_version is required'}"
     cmd_ssh "$_hex_bak_server" rm -v "hexblade/bak/$_hex_bak_name/$_hex_bak_version.tgz.gpg"
 }
 
-function cmd_bak_remote_ls() {
+function cmd_remote_ls() {
     local _hex_bak_server="${1?'_hex_bak_server is required, like user@host'}"
     cmd_ssh "$_hex_bak_server" find "hexblade/bak" -name "*.tgz.gpg"
 }
 
+function cmd_part_create() {
+    #sgdisk --backup=<file> <device>
+    #sgdisk --load-backup=<file> <device>
+    #gdisk -l <device>
+    true
+}
 
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"

ce629f07b41308fc2d0f23d7799f1a10bf1058c6
murer <murer@dextra-sw.com>
2022-01-20 16:30:48 -0300

    x

--- src/packs/backup/bak.sh
+++ src/packs/backup/bak.sh
@@ -14,10 +14,17 @@ cmd_bak_tar_create() {
 }
 
 cmd_bak_tar_restore() {
-    local _hex_bakfile="${1?'_hex_bakfile is required, you can use - to stdout'}"
+    local _hex_bakfile="${1?'_hex_bakfile is required, you can use - to stdin'}"
+    local _hex_cat="cat"
+    if [[ "x$_hex_bakfile" != "x-" ]]; then
+        _hex_bakfile="$(readlink -f "$_hex_bakfile")"
+        _hex_cat="pv"
+    fi
+
     cd /mnt/hexblade/basesys
-    sudo tar cpf - . | pv -s "$_hex_size" | gzip | \
-        gpg --batch -c --compress-algo none --passphrase-file "$HOME/.ssh/id_rsa" -o "$_hex_bakfile" - 
+    sudo "$_hex_cat" "$_hex_bakfile" | \
+        gpg --batch -d --compress-algo none --passphrase-file "$HOME/.ssh/id_rsa" -o - - | \
+        gunzip | sudo tar xpf - 
     cd -
 
 }

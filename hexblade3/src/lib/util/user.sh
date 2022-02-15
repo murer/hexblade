a4f8da33813af7ea030f06c85167dc509dc74db6
murer <murer@dextra-sw.com>
2022-02-15 11:25:28 -0300

    x

--- hexblade3/src/lib/util/user.sh
+++ hexblade3/src/lib/util/user.sh
@@ -4,7 +4,7 @@ function cmd_add() {
     hexblade_username="${1?'user name is required'}"
     if [[ ! -d "/mnt/hexblade/installer/home/$hexblade_username" ]]; then
         hexblade_password="$2"
-        if [ "x$hexblade_password" == "x" ]; then
+        if [ "x$hexblade_password" != "x" ]; then
             hexblade_password="$(openssl passwd -6)"
         fi
         arch-chroot /mnt/hexblade/system useradd -m -G adm,cdrom,sudo,dip,plugdev -s /bin/bash "$hexblade_username" -p "$hexblade_password"

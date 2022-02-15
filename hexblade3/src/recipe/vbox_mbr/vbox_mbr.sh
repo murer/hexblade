ad902a342b551562183e4a10d11e9598c72f07d7
murer <murer@dextra-sw.com>
2022-02-14 23:15:11 -0300

    x

--- hexblade3/src/recipe/vbox_mbr/vbox_mbr.sh
+++ hexblade3/src/recipe/vbox_mbr/vbox_mbr.sh
@@ -4,7 +4,12 @@ cmd_install() {
     ../../lib/mbr.sh wipe /dev/sdb
     ../../lib/mbr.sh part_add /dev/sdb 1 0 0 0x83
     ../../lib/mkfs.sh ext4 /dev/sdb1 HEXBLADE
-    #sfdisk -d /dev/sdb
+
+    mkdir /mnt/hexblade
+    mkdir -p /mnt/hexblade/system
+    mount /dev/sdb1 /mnt/hexblade/system
+    
+    ../../lib/strap.sh strap br
 }
 
 set +x; cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; set -x; "cmd_${_cmd}" "$@"

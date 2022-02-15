648df012bb2e2667683f1c1b03110befb4277c63
murer <murer@dextra-sw.com>
2022-02-15 18:19:52 -0300

    x

--- hexblade3/src/recipe/util/live.sh
+++ hexblade3/src/recipe/util/live.sh
@@ -15,15 +15,21 @@ function cmd_base() {
     [[ -d /mnt/hexblade/system ]]
     ../../lib/basesys/basesys.sh hostname hex
     ../../lib/basesys/basesys.sh base
-    ../../lib/basesys/basesys.sh kernel
     ../../lib/util/user.sh add ubuntu '$6$mGAOvwh5CP.LymHW$LLJaJCOo8Odj0w9jFXhEWLs90YEuy/EES5nwiIWZkEEnhs5jnzDqd4y96qDk/c9euGzMc8oFWsUkykTTYbk1T.'
     ../../lib/util/installer.sh uchr hex sudo -E /installer/hexblade/pack/util/tools.sh install
+}
+
+function cmd_iso() {
+    ../../lib/util/live.sh install
+    ../../lib/util/live.sh compress
+    ../../lib/util/live.sh iso
 }
 
 function cmd_from_scratch() {
     cmd_mount
     cmd_strap
     cmd_base
+    cmd_iso
 #    ../../lib/util/installer.sh umount
 }
 

dbc2ca3150a034428d533468093ba7fd66845ab7
murer <murer@dextra-sw.com>
2022-02-15 11:58:26 -0300

    x

--- hexblade3/src/lib/util/fstab.sh
+++ hexblade3/src/lib/util/fstab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 function cmd_gen() {
-  genfstab -U /mnt/hexblade/system | tee /mnt/hexblade/installer/etc/fstab
+  genfstab -U /mnt/hexblade/system | tee /mnt/hexblade/system/etc/fstab
 }
 
 set +x; cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; set -x; "cmd_${_cmd}" "$@"

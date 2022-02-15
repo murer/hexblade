4f522b1f8ce23de35454b9cd1c2e53819306e46a
murer <murer@dextra-sw.com>
2022-02-14 23:04:55 -0300

    x

--- hexblade3/src/lib/mkfs.sh
+++ hexblade3/src/lib/mkfs.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -e
 
 cmd_efi() {
-    local target_dev="${1?'device to be wipped'}"
+    local target_dev="${1?'device to be formatted'}"
     mkfs.fat -n ESP -F32 "$target_dev"  
+}
+
+cmd_ext4() {
+    local target_dev="${1?'device to be formatted'}"
+    local target_label="${2?'label'}"
+    yes | mkfs.ext4 -L "$target_label" "$target_dev"  
 }
 
 set +x; cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; set -x; "cmd_${_cmd}" "$@"
--- hexblade3/src/recipe/vbox_mbr/vbox_mbr.sh
+++ hexblade3/src/recipe/vbox_mbr/vbox_mbr.sh
@@ -2,9 +2,8 @@
 
 cmd_install() {
     ../../lib/mbr.sh wipe /dev/sdb
-    ../../lib/mbr.sh part_add /dev/sdb 2 0 +2G 0x83
-    ../../lib/mbr.sh part_add /dev/sdb 3 0 0 0x83
-
+    ../../lib/mbr.sh part_add /dev/sdb 1 0 0 0x83
+    ../../lib/mkfs.sh ext4 /dev/sdb1 HEXBLADE
     #sfdisk -d /dev/sdb
 }
 

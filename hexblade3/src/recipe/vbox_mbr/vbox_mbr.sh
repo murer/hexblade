7de48ac8a47a1d7144c38e05cc6db46ecea4bdba
murer <murer@dextra-sw.com>
2022-02-14 23:29:06 -0300

    x

--- hexblade3/src/recipe/vbox_mbr/vbox_mbr.sh
+++ hexblade3/src/recipe/vbox_mbr/vbox_mbr.sh
@@ -1,15 +1,23 @@
 #!/bin/bash -e
 
+function cmd_config_check() {
+    [[ "x$HEX_TARGET_DEV" != "x" ]]
+}
+
 function cmd_install() {
-    ../../lib/mbr.sh wipe /dev/sdb
-    ../../lib/mbr.sh part_add /dev/sdb 1 0 0 0x83
-    ../../lib/mkfs.sh ext4 /dev/sdb1 HEXBLADE
+    cmd_config_check
+
+    ../../lib/mbr.sh wipe "$HEX_TARGET_DEV"
+    ../../lib/mbr.sh part_add "$HEX_TARGET_DEV" 1 0 0 0x83
+    ../../lib/mkfs.sh ext4 "${HEX_TARGET_DEV}1" HEXBLADE
 
     mkdir /mnt/hexblade
     mkdir -p /mnt/hexblade/system
-    mount /dev/sdb1 /mnt/hexblade/system
+    mount "${HEX_TARGET_DEV}1" /mnt/hexblade/system
     
     ../../lib/strap.sh strap br
 }
+
+
 
 set +x; cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; set -x; "cmd_${_cmd}" "$@"

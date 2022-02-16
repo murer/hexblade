7b91703d7a8eaa78634f7e61accfa9674e4158a0
murer <murer@dextra-sw.com>
2022-02-15 23:22:44 -0300

    x

--- hexblade3/src/recipe/util/min_crypt_mbr.sh
+++ hexblade3/src/recipe/util/min_crypt_mbr.sh
@@ -38,7 +38,7 @@ function cmd_base() {
 
 function cmd_boot() {
     [[ "x$HEX_TARGET_DEV" != "x" ]]
-    ../../lib/util/crypt.sh crypttab
+    ../../lib/util/crypt.sh crypttab MAINCRYPTED
     ../../lib/util/fstab.sh gen
     ../../lib/util/boot.sh boot "$HEX_TARGET_DEV"
 }

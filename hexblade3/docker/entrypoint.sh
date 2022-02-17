b0cb11794a19495bed425ed510a52fbdc038d2f3
murer <murer@dextra-sw.com>
2022-02-17 17:12:49 -0300

    x

--- hexblade3/docker/entrypoint.sh
+++ hexblade3/docker/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -xe
 
+arg="${1?'one argument is required, use hexbladestart to start only'}"
+
+if [[ "x$arg" != "xhexbladestart" ]]; then
+    (printf "'%s' " "$@" && echo ' &') | sudo tee /etc/xdg/openbox/autostart.d/99-dockercmd.sh
+    sudo chmod 644 /etc/xdg/openbox/autostart.d/99-dockercmd.sh
+fi
+
 xvfb-run -s "$DISPLAY" -s '-screen 0 1024x700x24 -ac' openbox-session

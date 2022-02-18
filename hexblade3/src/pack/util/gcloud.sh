faad7cef1916b2c158f0de4eadac4128ed51c382
murer <murer@dextra-sw.com>
2022-02-18 11:10:14 -0300

    x

--- hexblade3/src/pack/util/gcloud.sh
+++ hexblade3/src/pack/util/gcloud.sh
@@ -7,7 +7,11 @@ function cmd_install() {
   echo "deb [signed-by=/etc/apt/hardkeys/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" > /etc/apt/sources.list.d/google-cloud-sdk.list
   apt -y update
   apt install -y google-cloud-sdk
-  gcloud config set disable_usage_reporting false
+  if [[ "x$SUDO_USER" == "x" ]]; then
+    gcloud config set disable_usage_reporting false
+  else
+    sudo -u "$SUDO_USER" gcloud config set disable_usage_reporting false
+  fi
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"

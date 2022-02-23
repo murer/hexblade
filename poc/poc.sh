47ebf947aa8c03de52b1c377341119a6d41523be
murer <murer@dextra-sw.com>
2022-02-23 15:53:52 -0300

    x

--- poc/poc.sh
+++ poc/poc.sh
@@ -9,7 +9,10 @@ function cmd_vm() {
   vboxmanage modifyvm test --nic1 nat 
   vboxmanage storagectl test --name IDE --add ide
   vboxmanage storageattach test --storagectl IDE --port 0 --device 0 --type dvddrive --medium /mnt/hexblade/iso/hexblade.iso
+  vboxmanage modifyvm test --natpf1 "guestssh,tcp,,2222,,22"
   vboxmanage startvm test --type gui -E aaa=bbb
+
+  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=5 -o ConnectionAttempts=1000 -p 2222 ubuntu@localhost whoami
 }
 
 set +x; cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; set -x; "cmd_${_cmd}" "$@"

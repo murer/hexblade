dc7558f69fd21e9a04802ebd31a208de2c700f25
murer <murer@dextra-sw.com>
2022-05-02 09:56:47 -0300

    x

--- docker/entrypoint/entrypoint.sh
+++ docker/entrypoint/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -xe
 
 ls /opt/hexblade/docker/entrypoint/entrypoint.d | sort | while read k; do
-    echo "Exec: $k"
-    . "/opt/hexblade/docker/entrypoint/entrypoint.d/$k"
+    echo "Exec: $k" 1>&2
+    . "/opt/hexblade/docker/entrypoint/entrypoint.d/$k" 1>&2
 done
+
+. /opt/hexblade/docker/entrypoint/start.sh

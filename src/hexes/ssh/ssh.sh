ccfbeac797a65cfe93e30bef97ea88424576a142
murer <murer@ciandt.com>
2022-05-26 15:45:30 -0300

    x

--- src/hexes/ssh/ssh.sh
+++ src/hexes/ssh/ssh.sh
@@ -1,19 +1,22 @@
 #!/bin/bash -xe
 
 function cmd_key_save() {
-  local _basedir="$(pwd)"
+  local hex_profile="${1?'hex_profile is required'}"
+  local _basedir="$(pwd)/$hex_profile"
   [[ ! -f "$_basedir/ssh-key.tar.gz.gpg" ]]
   cd "$HOME"
   [[ -d .ssh ]]
-  tar czf - .ssh | gpg --batch -c --armor -o "$_basedir/ssh-key.tar.gz.gpg" -
+  mkdir -p "$_basedir"
+  tar czf - .ssh | gpg --pinentry-mode loopback -c --armor -o "$_basedir/ssh-key.tar.gz.gpg" -
   cd -
 }
 
-function cmd_key_load() {
-  local  _basedir="$(pwd)"
+function cmd_key_load() {  
+  local hex_profile="${1?'hex_profile is required'}"
+  local _basedir="$(pwd)/$hex_profile"
   cd "$HOME"
   [[ ! -d .ssh ]]
-  gpg --batch -d "$_basedir/ssh-key.tar.gz.gpg" | tar xz --no-same-owner -f -
+  gpg --pinentry-mode loopback -d "$_basedir/ssh-key.tar.gz.gpg" | tar xz --no-same-owner -f -
   cd -
 }
 

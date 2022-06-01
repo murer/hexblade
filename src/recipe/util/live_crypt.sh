4251ce9a2c423a1ab1868dea1d8862c0a9d36900
murer <murer@ciandt.com>
2022-06-01 17:30:11 -0300

    x

--- src/recipe/util/live_crypt.sh
+++ src/recipe/util/live_crypt.sh
@@ -5,8 +5,8 @@ function cmd_from_iso() {
     ../../lib/iso/iso.sh deiso "$hexblade_iso"
     # ../../lib/iso/iso.sh decompress
     # ../../lib/util/installer.sh chr passwd ubuntu
-    ../../lib/iso/iso.sh iso
-    ../../lib/iso/iso.sh sha256
+    # ../../lib/iso/iso.sh iso
+    # ../../lib/iso/iso.sh sha256
 }
 
 set +x; cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; set -x; "cmd_${_cmd}" "$@"

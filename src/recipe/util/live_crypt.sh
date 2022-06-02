f74e371fbd1df25846f87ccaf0199a706b628a70
murer <murer@ciandt.com>
2022-06-02 09:21:52 -0300

    x

--- src/recipe/util/live_crypt.sh
+++ src/recipe/util/live_crypt.sh
@@ -25,14 +25,55 @@ function cmd_disk() {
     ../../lib/util/crypt.sh dump "${HEX_TARGET_DEV}2"
 }
 
-function cmd_from_iso() {
+function cmd_deiso() {
     local hexblade_iso="${1?'iso file'}"
     ../../lib/iso/iso.sh deiso "$hexblade_iso"
+    ../../lib/iso/iso.sh decompress    
+}
+
+function cmd_iso() {
+    HEXBLADE_LIVE_DISABLE_ADDUSER=true ../../lib/iso/iso.sh install
+    ../../lib/iso/iso.sh compress
+    ../../lib/iso/iso.sh iso
+    ../../lib/iso/iso.sh sha256
+}
+
+function cmd_crypt_open() {
+    [[ "x$HEX_TARGET_DEV" != "x" ]]
+    ../../lib/util/crypt.sh open "${HEX_TARGET_DEV}2" LIVECRYPTED master
+    ../../lib/util/lvm.sh open LIVELVM 
+}
+
+function cmd_mount() {
+    [[ ! -d /mnt/hexblade/cryptiso ]]
+    cmd_crypt_open
+    mkdir -p /mnt/hexblade/cryptiso/efi
+    mount "${HEX_TARGET_DEV}1" /mnt/hexblade/cryptiso/efi
+    mkdir -p /mnt/hexblade/cryptiso/image
+    mount /dev/mapper/LIVELVM-LIVEROOT /mnt/hexblade/cryptiso/image
+}
+
+function cmd_crypt_close() {
+    ../../lib/util/lvm.sh close LIVELVM
+    ../../lib/util/crypt.sh close LIVECRYPTED
+}
+
+function cmd_umount() {
+    umount /mnt/hexblade/cryptiso/image
+    umount /mnt/hexblade/cryptiso/efi
+    rmdir /mnt/hexblade/cryptiso/image /mnt/hexblade/cryptiso/efi /mnt/hexblade/cryptiso
+    cmd_crypt_close
+}
+
+# function cmd_rsync() {
+#     rsync -av /mnt/hexblade/image/
+# }
+
+function cmd_from_iso() {
+    cmd_deiso "$@"
     cmd_disk
-    # ../../lib/iso/iso.sh decompress
     # ../../lib/util/installer.sh chr passwd ubuntu
-    # ../../lib/iso/iso.sh iso
-    # ../../lib/iso/iso.sh sha256
+    cmd_iso
 }
 
 set +x; cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; set -x; "cmd_${_cmd}" "$@"

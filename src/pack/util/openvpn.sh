2a6e8ceb8d72eb278a486878a4082f8f3b1ae82a
murer <murer@ciandt.com>
2022-07-19 22:17:39 -0300

    x

--- src/pack/util/openvpn.sh
+++ src/pack/util/openvpn.sh
@@ -3,8 +3,8 @@
 cmd_install() {
 	if ! openvpn --version  | grep "^OpenVPN"; then
 		mkdir -p /etc/apt/hardkeys
-		wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg > /etc/apt/hardkeys/openvpn.gpg
-        	echo "deb [signed-by=/etc/apt/hardkeys/openvpn.gpg] http://build.openvpn.net/debian/openvpn/stable focal main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
+		wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg | gpg --dearmor > /etc/apt/hardkeys/openvpn.gpg
+        	echo "deb [signed-by=/etc/apt/hardkeys/openvpn.gpg] http://build.openvpn.net/debian/openvpn/release/2.5 focal main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
         	apt-get update
 		apt-get install -y openvpn
 	fi

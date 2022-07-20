39e0312349f1153d4576e92a59513893a09bcfde
murer <murer@ciandt.com>
2022-07-19 22:19:17 -0300

    x

--- src/pack/util/chrome.sh
+++ src/pack/util/chrome.sh
@@ -2,8 +2,8 @@
 
 function cmd_install() {
     mkdir -p /etc/apt/hardkeys
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub > /etc/apt/hardkeys/google-chrome.list
-    echo 'deb [arch=amd64 signed-by=/etc/apt/hardkeys/google-chrome.list] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /etc/apt/hardkeys/google-chrome.gpg
+    echo 'deb [arch=amd64 signed-by=/etc/apt/hardkeys/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list
     apt update
     apt -y install google-chrome-stable
 }

743675909a2aea153f8d908b9909f3b6aec89ff5
murer <murer@ciandt.com>
2023-01-03 08:22:52 -0300

    x

---
+++ src/pack/openbox/etc/xdg/openbox/bright.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -xe
+
+cmd_up() {
+  brightnessctl set '+10000'
+  notify-send "$(brightnessctl)" 
+}
+
+cmd_down() {
+  brightnessctl get '10000-'
+  notify-send "$(brightnessctl)"
+}
+
+cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"
+

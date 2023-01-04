ce892b04018b206dd676dd32187934139d8e31d4
murer <murer@ciandt.com>
2023-01-04 19:01:44 -0300

    x

--- src/vbox/host/util/vbox-host-net.sh
+++ src/vbox/host/util/vbox-host-net.sh
@@ -23,27 +23,9 @@ function cmd_drop() {
 }
 
 function cmd_share_internet() {
-    # sudo iptables -A INPUT -i vboxnet0 -s 192.168.56.0/24 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
-    # sudo iptables -A OUTPUT -o vboxnet0 -d 192.168.56.0/24 -m state --state RELATED,ESTABLISHED -j ACCEPT
     sudo iptables -A FORWARD -s 192.168.56.0/24 ! -d 192.168.56.0/24 -i vboxnet0 -j ACCEPT
     sudo iptables -A FORWARD ! -s 192.168.56.0/24 -d 192.168.56.0/24 -o vboxnet0 -j ACCEPT
-    # sudo iptables -t nat -I POSTROUTING -o wlp0s20f3 -j MASQUERADE
     sudo iptables -t nat -I POSTROUTING -s 192.168.56.0/24 ! -o vboxnet0  -j MASQUERADE
-    
-    # sudo iptables -N VBOXNET0
-    # sudo iptables -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-    # sudo iptables -I FORWARD  -m state --state RELATED,ESTABLISHED -j ACCEPT
-    # sudo iptables -t nat -I POSTROUTING -o wlp0s20f3 -j MASQUERADE
-    # sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-    # sudo iptables -A VBOXNET0 -j RETURN
-    
-    # sudo iptables -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-    # sudo iptables -I FORWARD  -m state --state RELATED,ESTABLISHED -j ACCEPT
-
-    # sudo iptables -A PREROUTING -m addrtype --dst-type LOCAL -j ACCEPT
-    # sudo iptables -A OUTPUT ! -d 192.168.56.0/24 -m addrtype --dst-type LOCAL -j ACCEPT
-    # sudo iptables -A POSTROUTING -s 192.168.56.0/24 ! -o vboxnet0 -j MASQUERADE
-    # sudo iptables -A DOCKER -i vboxnet0 -j RETURN
 }
 
 set +x; cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; set -x; "cmd_${_cmd}" "$@"

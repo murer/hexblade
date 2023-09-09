069f8777959d40969575b4cd2744e82705dd5d0b
murer <murer@ciandt.com>
2023-09-09 02:38:13 -0300

    hex

--- src/recipe/util/live_crypt.sh
+++ src/recipe/util/live_crypt.sh
@@ -47,7 +47,6 @@ function cmd_customize() {
     mkdir -p /mnt/hexblade/system/livedata/hexes/root/etc /mnt/hexblade/system/livedata/home/ubuntu
     [ -d /mnt/hexblade/system/livedata/hexes/root/etc/NetworkManager ] || mv /mnt/hexblade/system/etc/NetworkManager /mnt/hexblade/system/livedata/hexes/root/etc
     rm -rf /mnt/hexblade/system/etc/NetworkManager || true 
-    bash
     arch-chroot /mnt/hexblade/system ln -s /livedata/hexes/root/etc/NetworkManager /etc/NetworkManager
     arch-chroot /mnt/hexblade/system chown -R ubuntu:ubuntu /livedata/home/ubuntu
     umount /mnt/hexblade/system/livedata

68471cc9b539610fd64483b97edac301eacbbd0b
murer <murer@ciandt.com>
2023-09-08 23:14:57 -0300

    hex

--- src/recipe/util/live_crypt.sh
+++ src/recipe/util/live_crypt.sh
@@ -45,7 +45,8 @@ function cmd_decrypt() {
     [ "x$hexblade_crypted_data" != "x" ]
 
     ../../lib/crypt/crypt.sh initramfs_cryptparts_append iso "/dev/disk/by-uuid/$hexblade_crypted_root" LIVECRYPTEDROOT
-    ../../lib/crypt/crypt.sh initramfs_cryptparts_append iso "/dev/disk/by-uuid/$hexblade_crypted_data" LIVECRYPTEDDATA
+    ../../lib/crypt/crypt.sh initramfs_cryptparts_append iso "/dev/disk/by-uuid/$hexblade_crypted_data" LIVECRYPTEDDATA 
+    ../../lib/crypt/crypt.sh "/dev/disk/by-uuid/$hexblade_crypted_data" /root/livedata -r
     ../../lib/util/boot.sh initramfs
     ../../lib/iso/iso.sh compress
     ../../lib/iso/iso.sh install

cb60624cd24f65fca94018b778c3ff1a8da7e97d
murer <murer@ciandt.com>
2024-03-29 13:12:14 -0300

    init

--- src/pack/util/gcloud.sh
+++ src/pack/util/gcloud.sh
@@ -3,7 +3,7 @@
 function cmd_install() {
   apt install -y apt-transport-https ca-certificates gnupg
   mkdir -p /etc/apt/hardkeys
-  curl "https://packages.cloud.google.com/apt/doc/apt-key.gpg" > /etc/apt/hardkeys/cloud.google.gpg
+  curl "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | gpg --dearmor  > /etc/apt/hardkeys/cloud.google.gpg
   echo "deb [signed-by=/etc/apt/hardkeys/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" > /etc/apt/sources.list.d/google-cloud-sdk.list
   apt -y update
   apt install -y google-cloud-sdk

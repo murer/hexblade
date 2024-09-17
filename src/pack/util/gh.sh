6ace090971f34efcd9ff9283f723520d723cb552
murer <murer@dextra-sw.com>
2024-09-17 10:39:14 -0300

    Update gh.sh

--- src/pack/util/gh.sh
+++ src/pack/util/gh.sh
@@ -3,18 +3,20 @@
 function cmd_install() {
   [[ "x$UID" == "x0" ]]
   
-  file="$(mktemp)"
+  file="$(mktemp -d)"
   _cleanup() {
-    rm "$file" || true
+    rm -rf "$file" || true
   }
   trap _cleanup EXIT
 
-  wget --progress=dot -e dotbytes=1M -c \
-    'https://github.com/cli/cli/releases/download/v1.7.0/gh_1.7.0_linux_386.tar.gz' \
-    -O "$file"
-
-  tar xzOf "$file" gh_1.7.0_linux_386/bin/gh > /usr/local/bin/gh
+  curl -s https://api.github.com/repos/cli/cli/releases/latest | \
+    jq -r '.assets[].browser_download_url' | \
+    grep linux_amd64 | grep 'tar.gz$' | \
+    xargs wget --progress=dot -e dotbytes=1M -c -O "$file/file.tar.gz"
+  tar xzCf "$file" "$file/file.tar.gz"
+  cp "$(find "$file" -name 'gh' | grep '/bin/gh$')" /usr/local/bin/gh
   chmod +x /usr/local/bin/gh
+
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"

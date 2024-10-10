3a95f204f9a6fe4596be7830c76c81872e04971f
murer <murer@sample>
2024-10-10 08:03:42 -0300

    x

--- src/hexes/util/luks.sh
+++ src/hexes/util/luks.sh
@@ -20,6 +20,7 @@ function cmd_pass_add() {
 
 function cmd_drop_slot() {
   local hexblade_crypt_dev="${1?'hexblade_crypt_dev is required'}"
+  local hexblade_crypt_slot="${2?'slot, from 0 to 7'}"
   cryptsetup -v luksKillSlot "$hexblade_crypt_dev" "$hexblade_crypt_slot"
 }
 

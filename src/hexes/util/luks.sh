1f235209c9a454193906de58b05e5adf53f5c899
murer <murer@sample>
2024-10-10 08:02:31 -0300

    x

--- src/hexes/util/luks.sh
+++ src/hexes/util/luks.sh
@@ -5,33 +5,22 @@ function cmd_dump() {
     cryptsetup luksDump "$hexblade_crypt_dev"
 }
 
-function cmd_adduser() {
-    [[ "x$UID" == "x0" ]]
-    local hexes_username="${1?'username'}"
-    useradd -m -G video -s /usr/sbin/nologin "$hexes_username"
-    echo "xhost '+SI:localuser:$hexes_username'" > "/etc/xdg/openbox/autostart.d/20-xhost-user-$hexes_username.sh"
-    xhost "+SI:localuser:$hexes_username"
+function cmd_pass_add_from_key() {
+  local hexblade_crypt_dev="${1?'hexblade_crypt_dev is required'}"
+  local hexblade_crypt_key="${2?'keyname, like: /etc/lukskeys/master.key'}"
+  local hexblade_crypt_slot="${3?'slot, from 0 to 7'}"
+  cryptsetup luksAddKey --key-file "$hexblade_crypt_key" --key-slot "$hexblade_crypt_slot" "$hexblade_crypt_dev"
 }
 
-function cmd_reuser() {
-    local hex_user="${1?'username'}"
-    shift
-    cd -
-    sudo -sHu "$hex_user" -g "$hex_user" \
-        NO_AT_BRIDGE=1 DISPLAY="$DISPLAY" PULSE_SERVER=127.0.0.1:4713 "$@"
+function cmd_pass_add() {
+  local hexblade_crypt_dev="${1?'hexblade_crypt_dev is required'}"
+  local hexblade_crypt_slot="${2?'slot, from 0 to 7'}"
+  cryptsetup luksAddKey --key-slot "$hexblade_crypt_slot" "$hexblade_crypt_dev"
 }
 
-function cmd_fork() {
-    local hex_user="${1?'username'}"
-    shift
-    cd -
-    sudo -sHu "$hex_user" -g "$hex_user" -b \
-        NO_AT_BRIDGE=1 DISPLAY="$DISPLAY" PULSE_SERVER=127.0.0.1:4713 "$@"
-}
-
-function cmd_install() {
-    [[ ! -f /usr/local/bin/reuser.sh ]] || [[ "x$1" == "x-f" ]]
-    cp -v reuser.sh /usr/local/bin/reuser.sh
+function cmd_drop_slot() {
+  local hexblade_crypt_dev="${1?'hexblade_crypt_dev is required'}"
+  cryptsetup -v luksKillSlot "$hexblade_crypt_dev" "$hexblade_crypt_slot"
 }
 
 set +x; cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; set -x; "cmd_${_cmd}" "$@"

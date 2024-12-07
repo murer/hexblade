2c2a1ff93a339cc6a20e16ef71520dfa32d7e1e5
murer <murer@sample>
2024-12-07 18:10:00 -0300

    x

--- src/vbox/vboxbase/vbox-base.sh
+++ src/vbox/vboxbase/vbox-base.sh
@@ -2,10 +2,16 @@
 
 
 function cmd_create() {
+    umount /mnt/hexbase/files || true
+    kpartx -dv /dev/loop5 || true
+    losetup -d /dev/loop5 || true
     [ -f /tmp/hexblade-base.vdi ]
-    mkdir -p /mnt/hexbase
-    [ ! -f /mnt/hexbase/my_disk_image.raw ] || rm /mnt/hexbase/my_disk_image.raw
-    qemu-img convert -f vdi -O raw /tmp/hexblade-base.vdi /mnt/hexbase/my_disk_image.raw
+    mkdir -p /mnt/hexbase/files
+    # [ ! -f /mnt/hexbase/my_disk_image.raw ] || rm /mnt/hexbase/my_disk_image.raw
+    # qemu-img convert -f vdi -O raw /tmp/hexblade-base.vdi /mnt/hexbase/my_disk_image.raw
+    losetup loop5 /mnt/hexbase/my_disk_image.raw
+    kpartx -arv /dev/loop5
+    mount /dev/mapper/loop5p1 /mnt/hexbase/files
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"

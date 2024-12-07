e20702c30f23248d83db96c8f4b25935376093cb
murer <murer@sample>
2024-12-07 19:02:57 -0300

    x

--- src/vbox/vboxbase/vbox-base.sh
+++ src/vbox/vboxbase/vbox-base.sh
@@ -2,6 +2,8 @@
 
 
 function cmd_create() {
+    rm /mnt/hexbase/out.vdi || true
+
     mkdir -p /mnt/hexbase/files /mnt/hexbase/out
     cp images/blank.vdi /mnt/hexbase/blank.vdi
 
@@ -9,8 +11,8 @@ function cmd_create() {
     kpartx -dv /dev/loop5 || true
     losetup -d /dev/loop5 || true
     [ -f /tmp/hexblade-base.vdi ]
-    # [ ! -f /mnt/hexbase/my_disk_image.raw ] || rm /mnt/hexbase/my_disk_image.raw
-    # qemu-img convert -f vdi -O raw /tmp/hexblade-base.vdi /mnt/hexbase/my_disk_image.raw
+    [ ! -f /mnt/hexbase/my_disk_image.raw ] || rm /mnt/hexbase/my_disk_image.raw
+    qemu-img convert -f vdi -O raw /tmp/hexblade-base.vdi /mnt/hexbase/my_disk_image.raw
     losetup loop5 /mnt/hexbase/my_disk_image.raw
     kpartx -arv /dev/loop5
     mount -r /dev/mapper/loop5p1 /mnt/hexbase/files
@@ -26,29 +28,20 @@ function cmd_create() {
     ../../lib/util/mkfs.sh ext4 /dev/mapper/loop6p1 SYS
     mount /dev/mapper/loop6p1 /mnt/hexbase/out
 
-    # VBoxManage closemedium disk /mnt/hexbase/hexblade.vdi --delete || true
-    # VBoxManage createhd --filename  /mnt/hexbase/hexblade.vdi --size 262144 --format VDI
+    rsync -a --delete /mnt/hexbase/files/ /mnt/hexbase/out/
+    arch-chroot /mnt/hexbase/out/ grub-install /dev/loop6
 
+    umount /mnt/hexbase/out
+    kpartx -dv /dev/loop6
+    losetup -d /dev/loop6
+ 
+    umount /mnt/hexbase/files
+    kpartx -dv /dev/loop5
+    losetup -d /dev/loop5
+    
+    qemu-img convert -f raw -O vdi /mnt/hexbase/out.raw /mnt/hexbase/out.vdi
 
-#     VBoxManage snapshot hex_one restore hexblade
-# VBoxManage snapshot personal2 restore hexblade
-
-# #VBoxManage storageattach personal2 --storagectl "SATA" --port 3 --device 0 --medium none || true
-# VBoxManage closemedium disk hex_one/hex.vdi --delete || true
-# [ ! -f hex_one/hex.vdi ]
-
-# #VBoxManage storageattach personal2 --storagectl "SATA" --port 2 --device 0 --medium none || true
-# VBoxManage closemedium disk tmp/hex-base.vdi --delete || true
-# [ ! -f tmp/hex-base.vdi ]
-
-# #VBoxManage clonehd ./hex_base_v1/hex_base_v1-disk1.vdi tmp/hex-base.vdi --format VDI
-# #VBoxManage storageattach personal2 --storagectl "SATA" --port 2 --device 0 --type hdd --medium ./hex_base_v1/hex_base_v1-disk1.vdi
-
-# [ ! -f tmp/hexblade.raw ] || rm tmp/hexblade.raw
-
-# qemu-img convert -f vdi -O raw ./hex_base_v1/hex_base_v1-disk1.vdi tmp/hexblade.raw
-# du -hs tmp/*
-
+    
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"

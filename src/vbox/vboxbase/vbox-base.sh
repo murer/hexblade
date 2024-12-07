f15c18b350542a3963f9b41e9e21d50a39002aef
murer <murer@sample>
2024-12-07 18:01:30 -0300

    x

--- src/vbox/vboxbase/vbox-base.sh
+++ src/vbox/vboxbase/vbox-base.sh
@@ -2,9 +2,10 @@
 
 
 function cmd_create() {
-    [ ! -f /tmp/hexblade-base.vdi ]
+    [ -f /tmp/hexblade-base.vdi ]
     mkdir -p /mnt/hexbase
-    qemu-img convert -f vdi -O raw /tmp/hexblade-base.vdi target/my_disk_image.raw
+    [ ! -f /mnt/hexbase/my_disk_image.raw ] || rm /mnt/hexbase/my_disk_image.raw
+    qemu-img convert -f vdi -O raw /tmp/hexblade-base.vdi /mnt/hexbase/my_disk_image.raw
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"

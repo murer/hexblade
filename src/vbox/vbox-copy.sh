5d7e250537fd614c475de3f8b6fa37a109d7900d
murer <murer@sample>
2024-12-07 17:56:48 -0300

    x

---
+++ src/vbox/vbox-copy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -xe
+
+VBoxManage guestcontrol personal2 copyto --target-directory /tmp/hexblade-base.vdi --username hex --password hex /ssd1/vbox/murer/vms/hex_base_v1/hex_base_v1-disk1.vdi
+

2eabd6ce006bbf8e4a8e045ad953143090d78501
murer <paulo.murer@infinity6.ai>
2025-03-13 12:18:39 -0300

    x

--- src/recipe/util/min_crypt_efi.sh
+++ src/recipe/util/min_crypt_efi.sh
@@ -69,6 +69,7 @@ function cmd_download_tcp() {
 function cmd_boot() {
     [[ "x$HEX_TARGET_DEV" != "x" ]]
     ../../lib/util/crypt.sh crypttab_start
+    ../../lib/util/crypt.sh crypttab_add MAINSWAP master
     ../../lib/util/crypt.sh crypttab_add MAINCRYPTED master
     ../../lib/util/fstab.sh gen
     ../../lib/util/boot.sh boot "$HEX_TARGET_DEV"

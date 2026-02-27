ac23652981ab963eb34fa3b378522297df934b97
paulo.murer <paulo.murer@infinity6.ai>
2026-02-27 23:09:15 +0000

    x

--- src/pack/util/weechat.sh
+++ src/pack/util/weechat.sh
@@ -7,10 +7,11 @@ function cmd_install() {
 }
 
 function cmd_config() {
-  weechat -a -r '/set irc.server_default.username "guest46723678"; /quit'
+  weechat -a -r '/set irc.server_default.username libera"guest46723678"; /quit'
   weechat -a -r '/set irc.server_default.realname "guest46723678"; /quit'
   weechat -a -r '/set irc.server_default.nicks "guest46723678"; /quit' 
-  weechat -a -r '/server del libera; /quit'
+  
+  #weechat -a -r '/server del libera; /quit'
   weechat -a -r '/server add libera irc.libera.chat/6697 -ssl; /quit'
 
   local _libera_user="$(cat "$HOME/.ssh/irc/libera.chat/username.txt")"
@@ -18,12 +19,11 @@ function cmd_config() {
     local _libera_pass="$(cat "$HOME/.ssh/irc/libera.chat/password.txt")"
     weechat -a -r "/set irc.server.libera.username \"$_libera_user\"; /quit"
     weechat -a -r "/set irc.server.libera.realname \"$_libera_user\"; /quit"
-    weechat -a -r "/set irc.server.libera.nicks \"$_libera_user\"; /quit'"
+    weechat -a -r "/set irc.server.libera.nicks \"$_libera_user\"; /quit"
+    weechat -a -r "/set irc.server.libera.sasl_mechanism plain; /quit"
+    weechat -a -r "/set irc.server.libera.sasl_username \"$_libera_user\"; /quit"
+    weechat -a -r "/set irc.server.libera.sasl_password \"$_libera_password\"; /quit"
   fi
-# /set irc.server.libera.sasl_mechanism plain
-# /set irc.server.libera.sasl_username <nickname>
-# /set irc.server.libera.sasl_password <password>
-#/save
   
 }
 

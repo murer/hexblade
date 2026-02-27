b9088166b6a535d9a91ed3b8ee0101ad363cdce3
paulo.murer <paulo.murer@infinity6.ai>
2026-02-27 22:25:59 +0000

    x

--- src/pack/util/weechat.sh
+++ src/pack/util/weechat.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -xe
 
 function cmd_install() {
-  [[ "x$UID" == "x0" ]]
-  
-  apt install -y weechat
+  [ "x$UID" == "x0" ]
+  weechat --version || apt install -y weechat
+  [ "x$SUDO_USER" == "x" ] || sudo -u "$SUDO_USER" ./weechat.sh config
+}
 
-  weechat -a -r '/server del libera; /quit'
-  weechat -a -r '/server add libera irc.libera.chat/6697 -ssl'
-  
+function cmd_config() {
+  echo weechat -a -r '/server del libera; /quit'
+  echo weechat -a -r '/server add libera irc.libera.chat/6697 -ssl; /quit'
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"

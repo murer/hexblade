8051f72e12299c69939d86547f67d8a33914fb5f
murer <842157+murer@users.noreply.github.com>
2026-03-01 15:54:34 -0300

    x

--- src/pack/util/proton-vpn-client.sh
+++ src/pack/util/proton-vpn-client.sh
@@ -3,7 +3,7 @@
 function cmd_install() {
     mkdir -p /etc/apt/hardkeys
     wget -q -O - https://repo.protonvpn.com/debian/public_key.asc | gpg --dearmor > /etc/apt/hardkeys/protonvpn-stable-archive-keyring.gpg
-    echo "deb [arch=all signed-by=/etc/apt/hardkeys/protonvpn-stable-archive-keyring.gpg] https://repo.protonvpn.com/debian stable main" > /etc/apt/sources.list.d/proton-vpn.list
+    echo "deb [signed-by=/etc/apt/hardkeys/protonvpn-stable-archive-keyring.gpg] https://repo.protonvpn.com/debian stable main" > /etc/apt/sources.list.d/proton-vpn.list
     apt update
     apt -y install proton-vpn-gnome-desktop
 }

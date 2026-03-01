bab1524c9aea33a6ba4c3bf62f9ab6366532090b
murer <murer@sample>
2026-03-01 18:04:15 -0300

    x

--- src/pack/protonvpn/proton-vpn-client.sh
+++ src/pack/protonvpn/proton-vpn-client.sh
@@ -1,17 +1,23 @@
 #!/bin/bash -xe
 
+function cmd_group() {
+  usermod -aG netdev "${1?'uername'}"
+}
+
 function cmd_install() {
     mkdir -p /etc/apt/hardkeys
     wget -q -O - https://repo.protonvpn.com/debian/public_key.asc | gpg --dearmor > /etc/apt/hardkeys/protonvpn-stable-archive-keyring.gpg
     echo "deb [signed-by=/etc/apt/hardkeys/protonvpn-stable-archive-keyring.gpg] https://repo.protonvpn.com/debian stable main" > /etc/apt/sources.list.d/proton-vpn.list
     apt update
     apt -y install proton-vpn-gtk-app #proton-vpn-gnome-desktop proton-vpn-cli
-}
 
-function cmd_config() {
-    protonvpn config set ipv6 off
-    protonvpn config set kill-switch standard
-    [ "x$USER" == "xroot" ] || sudo usermod -aG netdev "$USER"
+    if [[ "x$hexblade_user" != "x" ]]; then
+        cmd_group "$hexblade_user"
+    elif [[ "x$UID" == "x0" && "x$SUDO_USER" != "x" && "x$SUDO_UID" != "x0" ]]; then
+        cmd_group "$SUDO_USER"
+    elif [[ "x$UID" != "x0" ]]; then
+        cmd_group "$USER"
+    fi
 }
 
 set +x; cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; set -x; "cmd_${_cmd}" "$@"

2986de5681c605f3d18c9ba9489f7581ae279b8a
murer <842157+murer@users.noreply.github.com>
2026-03-01 15:51:57 -0300

    x

--- src/pack/util/proton-vpn-client.sh
+++ src/pack/util/proton-vpn-client.sh
@@ -5,7 +5,7 @@ function cmd_install() {
     wget -q -O - https://repo.protonvpn.com/debian/public_key.asc | gpg --dearmor > /etc/apt/hardkeys/protonvpn-stable-archive-keyring.gpg
     echo "deb [arch=all signed-by=/etc/apt/hardkeys/protonvpn-stable-archive-keyring.gpg] https://repo.protonvpn.com/debian stable main" > /etc/apt/sources.list.d/proton-vpn.list
     apt update
-    # apt -y install google-chrome-stable
+    apt -y install proton-vpn-gnome-desktop
 }
 
 set +x; cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; set -x; "cmd_${_cmd}" "$@"
